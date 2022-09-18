0a852e0c47c0ea2c2a4be7a946630246be052d18
Bruna <bruna.oflima@gmail.com>
2022-09-18 16:06:12 -0300

    binary gap task

---
+++ src/main/java/test_2/BinaryGapTest.java
@@ -0,0 +1,11 @@
+package test_2;
+
+public class BinaryGapTest {
+    public static void main(String[] args) {
+        BinaryGap binaryGap = new BinaryGap();
+
+        int A = 8;
+        int binaryGapSolution = binaryGap.solution(A);
+        System.out.print("Binary gap is: " + binaryGapSolution);
+    }
+}
